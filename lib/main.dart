5e143da5f459a79934d7e32a2e6c6cbc437d47d1
Deepak Soni <deepaksoni1010101@gmail.com>
2022-06-27 09:58:10 +0530

    Done

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:loginapp/signup.dart';
+
+import 'login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'login',
+    routes: {
+      'login': (context) => const MyLogin(),
+      'signup': (context) => const MySignUp(),
+    },
+  ));
+}
