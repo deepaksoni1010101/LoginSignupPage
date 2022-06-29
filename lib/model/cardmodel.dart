1aed8b013e81c27c69cc3c01c0f54e222e3ad151
Deepak Soni <deepaksoni1010101@gmail.com>
2022-06-29 14:05:03 +0530

    my profile pic

--- lib/model/cardmodel.dart
+++ lib/model/cardmodel.dart
@@ -6,10 +6,9 @@ class MyData {
 }
 
 List<MyData> datalist = [
-  MyData("Prakash", "asserts/deepak.jpg"),
-  MyData("Deepak", "asserts/login2.jpg"),
-  MyData("Suraj", "asserts/signup.jpg"),
-  MyData("Rahul", "asserts/signup.jpg"),
-  MyData("Prakhar", "asserts/signup.jpg"),
-  MyData("Umang", "asserts/signup.jpg")
+  MyData("Deepak", "asserts/deepak.jpg"),
+  MyData("Suraj", "https://avatars.githubusercontent.com/u/73026702?v=4"),
+  MyData("Rahul",
+      "https://media-exp1.licdn.com/dms/image/C4D03AQFEinTZ9JQbcw/profile-displayphoto-shrink_200_200/0/1648991671046?e=1661990400&v=beta&t=yTueQciZOAj_7blmUwjxd1e-QHmvvSFc4YMRIMnMULg"),
+  MyData("Prakhar", "https://avatars.githubusercontent.com/u/73019477?v=4"),
 ];
