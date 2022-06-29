c0cea791ee29ffba96b94a0558f0ac54669bfc1d
Deepak Soni <deepaksoni1010101@gmail.com>
2022-06-29 13:37:42 +0530

    List View Builder

---
+++ lib/model/cardmodel.dart
@@ -0,0 +1,15 @@
+class MyData {
+  String name;
+  String image;
+
+  MyData(this.name, this.image);
+}
+
+List<MyData> datalist = [
+  MyData("Prakash", "asserts/deepak.jpg"),
+  MyData("Deepak", "asserts/login2.jpg"),
+  MyData("Suraj", "asserts/signup.jpg"),
+  MyData("Rahul", "asserts/signup.jpg"),
+  MyData("Prakhar", "asserts/signup.jpg"),
+  MyData("Umang", "asserts/signup.jpg")
+];
--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loginapp/model/cardmodel.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -21,62 +22,67 @@ class _ProfilePageState extends State<ProfilePage> {
             centerTitle: true,
           ),
           body: Center(
-            child: Card(
-              elevation: 100,
-              shadowColor: Colors.black,
-              color: Color.fromARGB(255, 249, 206, 221),
-              child: SizedBox(
-                width: 300,
-                height: 500,
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(children: [
-                    CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 255, 124, 168),
-                      radius: 108,
-                      child: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w524-h640/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg"),
-                        radius: 100,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Deepak Soni',
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Color.fromARGB(255, 181, 30, 111),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                        'I am a Computer Science Student Studying in Chouksey Engineering College Bilaspur. I am Learing Android App Development in Flutter in Nable Invent Solutions. Thank You',
-                        style: TextStyle(
-                            fontSize: 17,
-                            color: Colors.black45,
-                            fontStyle: FontStyle.italic,
-                            fontWeight: FontWeight.w500)),
-                    SizedBox(
-                      width: 100,
-                      child: ElevatedButton(
-                        onPressed: () => 'Null',
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Color.fromARGB(255, 255, 128, 128))),
-                        child: Padding(
-                          padding: EdgeInsets.all(4),
-                          child: Row(
-                            children: [
-                              Icon(Icons.touch_app),
-                              Text('Visit'),
-                            ],
-                          ),
+            child: ListView.builder(
+              itemCount: datalist.length,
+              itemBuilder: (context, index) => Card(
+                elevation: 100,
+                shadowColor: Colors.black,
+                color: Color.fromARGB(255, 249, 206, 221),
+                child: SizedBox(
+                  width: 300,
+                  height: 500,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(children: [
+                      CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 255, 124, 168),
+                        radius: 108,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage(datalist[index].image),
+
+                          // NetworkImage(
+                          // "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w524-h640/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg"),
+                          radius: 100,
                         ),
                       ),
-                    )
-                  ]),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        datalist[index].name,
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 181, 30, 111),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                          'I am a Computer Science Student Studying in Chouksey Engineering College Bilaspur. I am Learing Android App Development in Flutter in Nable Invent Solutions. Thank You',
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.black45,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.w500)),
+                      SizedBox(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () => 'Null',
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Color.fromARGB(255, 255, 128, 128))),
+                          child: Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Row(
+                              children: [
+                                Icon(Icons.touch_app),
+                                Text('Visit'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+                  ),
                 ),
               ),
             ),
